1402c1809aa4e6f4beb5b4c40c4c1905e9219096
Nobel <nobelfix93@gmail.com>
2026-04-15 03:08:00 +0200

    set the reponsiveness

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -6,8 +6,9 @@ import hero from '../../assets/images/hero.png'
 const Banner = () => {
   return (
     <div className="w-6/12 mx-auto">
-      <div>
-        <h1 className="text-center mb-5 mt-10 font-inter text-[72px] font-bold leading-[84px] ">
+      <div className="flex flex-col-reverse md:flex-col lg:flex-col">
+        <div>
+          <h1 className="text-center mb-5 mt-10 font-inter text-[40px] md:text-[50px] lg:text-[72px] font-bold leading-[50px] md:leading-[60px] lg:leading-[84px] ">
           We Build <br /> <span className="text-[#9F62F2]">Productive</span> Apps
         </h1>
         <small className="text-center block">
@@ -15,13 +16,14 @@ const Banner = () => {
           simpler, smarter, and more exciting. Our goal is to turn your ideas
           into digital experiences that truly make an impact.
         </small>
+        </div>
 
-        <div className="flex items-center gap-10 mt-5 justify-center">
-          <li className="flex items-center gap-2">
-            <FaGooglePlay /> <span className="font-medium">Google Play</span>
+        <div className="flex items-center gap-5 md:gap-8 lg:gap-10-5 justify-center flex-nowrap">
+          <li className="flex items-center gap-2 whitespace-nowrap mt-5 md:mt-0 lg:mt-0">
+            <FaGooglePlay /> <span className="font-medium ">Google Play</span>
           </li>
-          <li className="flex items-center gap-2">
-            <FaAppStoreIos /> <span className="font-medium">App Store</span>
+          <li className="flex items-center gap-2 whitespace-nowrap mt-5 md:mt-0 lg:mt-0">
+            <FaAppStoreIos /> <span className="font-medium ">App Store</span>
           </li>
         </div>
       </div>
