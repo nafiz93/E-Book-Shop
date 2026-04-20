86013cd27adb1429d676e3d8db713ae55266e951
Nobel <nobelfix93@gmail.com>
2026-04-20 23:38:56 +0200

    set the hight of the card and then set the total number of the apps

--- src/components/allproducts/Allproduct.jsx
+++ src/components/allproducts/Allproduct.jsx
@@ -5,15 +5,17 @@ const Allproduct = ({ promise }) => {
 
     const data=use(promise);
 
-   // console.log(data);
+   
     return (
         <div>
 
-             <div>
-                <h1>Our All Applications</h1>
-                <p>Explore All Apps on the Market developed by us. We code for Millions</p>
+             <div className='w-6/12 mx-auto mb-10 mt-5'>
+                <h1 className='text-center font-sans text-5xl font-bold mb-3'>Our All Applications</h1>
+                <p className='text-center text-[#627382] font-sans text-xl'>Explore All Apps on the Market developed by us. We code for Millions</p>
             </div>
-<div className='grid grid-cols-3'>
+
+            <h1 className='ms-5 text-xl font-bold'>{`(${data.length}) Apps Found`}</h1>
+<div className='grid grid-cols-3 gap-5'>
     
            {
             data.map(product=><SingleAllProduct key={product.id} product={product}></SingleAllProduct>)
--- src/components/singleallproduct/SingleAllProduct.jsx
+++ src/components/singleallproduct/SingleAllProduct.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { formatMillions } from "../../utilities/Utilities";
 import { Download } from 'lucide-react';
+import { FaStar } from "react-icons/fa";
 const SingleAllProduct = ({ product }) => {
 
-    //console.log(product);
+   // console.log(product);
 
-    const {downloads,ratingAvg,image,title}= product;
+    const {downloads,ratingAvg,image,title,description}= product;
 
    console.log(downloads,ratingAvg,image);
   return (
     <div>
       <div>
-        <div className="card bg-base-100 w-96 shadow-sm">
-          <figure>
-            <img
+        <div className="card bg-base-100 w-96 shadow-lg">
+          <figure className="w-8/12 mx-auto">
+            <img 
+            className="w-full h-full  object-cover"
               src={image}
               alt="Shoes"
             />
@@ -23,13 +25,12 @@ const SingleAllProduct = ({ product }) => {
               {title}
              
             </h2>
-            <p>
-              A card component has a figure, a body part, and inside body there
-              are title and actions parts
+            <p classname="text-sm text-gray-600" font-sans>
+              {description}
             </p>
             <div className="card-actions flex justify-between">
-              <div className="flex gap-1"> <Download  className="text-purple-600" />{formatMillions(downloads)}</div>
-              <div className="badge badge-outline">{ratingAvg}</div>
+              <div className="flex gap-1 text-blue-500 font-bold shadow-lg"> <Download  className="text-blue-500 size-5"/>{formatMillions(downloads)}</div>
+              <div className="badge shadow-lg"> <FaStar className="text-yellow-400" />{ratingAvg}</div>
             </div>
           </div>
         </div>
